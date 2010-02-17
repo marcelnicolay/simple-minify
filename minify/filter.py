844ef74443f5b930144f7617f1eb0832b1a43f4c
marcel nicolay <marcel.nicolay@gmail.com>
2010-02-17 21:27:21 -0200

    filter file

--- minify/filter.py
+++ minify/filter.py
@@ -58,44 +58,55 @@ class FilterTemplate():
         for fname in dirlist:
         	search = re.search("(?P<name>[^\.]*)\.(?P<extension>.*)$",fname)
         	if search and search.groupdict()['extension'] == 'tpl':
-         		content = self.read("%s/%s" % (self.template_path, fname))
-	        	css_files, js_files = self.parse(content)
+				template_file = "%s/%s" % (self.template_path, fname)
+
+         		css_files, js_files = self.filter(template_file)
+	        	
 	        	output_css = "%s/minify/%s.css" % (self.css_output, search.groupdict()['name'])
 	        	output_js = "%s/minify/%s.js" % (self.js_output, search.groupdict()['name'])
 	        	
         		minify.add_group(files=css_files, output=output_css, root=self.media_dir )
-        		minify.add_group(files=js_files, output=output_js, root=self.media_dir)
+        		minify.add_group(files=js_files, output=output_js, root=self.media_dir )
+
+	        	self.parse(template_file, output_css, output_js)
 		
-		minify.minimalize()        
-        	
-    def parse(self, content):
-    	return self.parse_css(content), self.parse_js(content)
-    
-    def parse_css(self, content):
-    
+		minify.minimalize()
+
+    def write(self, file, content):
+        f = open(file, "w")
+        try:
+            f.write(content)
+        finally:
+            f.close()
+		
+	def filter(self, template_file):
+		content = self.read(template_file)
+		
 		css_files = []
+		js_files = []
 
 		links = re.findall('\<link[^>]*/>',content)
+		scripts = re.findall('\<script[^>]*></script>',content)
+
 		for link in links:
-			search = re.search('href="(?P<value>[^"]*)"', link)
-			
+			search = re.search('href="(?P<value>[^"]*)"', link)			
 			if search:
 				css_files.append(search.groupdict()['value'])
 
-		return css_files
-
-    def parse_js(self, content):
-    
-		js_files = []
-
-		scripts = re.findall('\<script[^>]*></script>',content)
 		for script in scripts:
 			search = re.search('src="(?P<value>[^"]*)"', script)
 			if search:
 				js_files.append(search.groupdict()['value'])
+		
+		return css_files, js_files	
 
-		return js_files
-
-            
-    
-    
+	def parse(self, template_file, output_css, output_js):	
+     	content = self.read(template_file)
+     	
+     	content = re.sub("\<link[^>]*/>", "", content)
+     	content = re.sub("\<script[^>]*></script>", "", content)
+     	
+     	content = re.sub("</head>", '<link type="text/css" href="%s" rel="stylesheet" />\n</head>' % output_css, content)
+     	content = re.sub("</body>", '<script type="text/javscript" src="%s" ></script>\n</body>' % output_js, content)
+     	
+     	self.write(template_file, content)
